ecce086d1488ca8602f892751a92a205621d05a9
Muhammad Usman Naeem <76055167+usman-naeem728@users.noreply.github.com>
2023-10-09 09:11:56 -0700

    redux using bindwithactioncreators

--- src/Shop.js
+++ src/Shop.js
@@ -1,13 +1,20 @@
 import React from 'react'
 import { useDispatch } from 'react-redux'
 import { actioncreators } from './state'
+import { bindActionCreators } from 'redux'
 
 const Shop = () => {
   const dispatch = useDispatch()
+  const {withdrawtMoney, depositMoney} = bindActionCreators(actioncreators,dispatch)
   return (
     <div>
-        <button className="btn btn-outline-success" onClick={()=>{dispatch(actioncreators.depositMoney(10))}}>+</button>
+        {/* <button className="btn btn-outline-success" onClick={()=>{dispatch(actioncreators.depositMoney(10))}}>+</button>
         <button className="btn btn-outline-success" onClick={()=>{dispatch(actioncreators.withdrawtMoney(10))}}>-</button>
+         */}
+        {/* ///with bind action creator */}
+        <button className="btn btn-outline-success" onClick={()=>{depositMoney(10)}}>+</button>
+        <button className="btn btn-outline-success" onClick={()=>{withdrawtMoney(10)}}>-</button>
+        
       
     </div>
   )
